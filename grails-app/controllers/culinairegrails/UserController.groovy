0986f1c80b8e75715d7cc5d8c30ccc5a6b8038fb
afdev <afdev@unal.edu.co>
2017-05-24 16:16:17 -0500

    Verificación de login con LDAP

--- grails-app/controllers/culinairegrails/UserController.groovy
+++ grails-app/controllers/culinairegrails/UserController.groovy
@@ -1,6 +1,5 @@
 package culinairegrails
 
-import com.novell.*
 import com.novell.ldap.LDAPConnection
 import com.novell.ldap.LDAPException
 
@@ -43,50 +42,55 @@ class UserController {
     def login(){
         String aux = login2(params.usernameL, params.passwordL)
         println("Login " + aux)
-        def person = Person.findByUsername(params.usernameL)
-        if (person) {
-            if(person.password == params.passwordL){
-                session.user = person.username
-                session.tu = true
-                redirect controller: 'web', action: 'perfil'
-                return
-            }else{
-                redirect controller: 'web', action: 'ingresar'
-                flash.message = "Contraseña incorrecta"
-                return
+        if(aux.equals("Usuario")) {
+            def person = Person.findByUsername(params.usernameL)
+            if (person) {
+                if (person.password == params.passwordL) {
+                    session.user = person.username
+                    session.tu = true
+                    redirect controller: 'web', action: 'perfil'
+                    return
+                } else {
+                    redirect controller: 'web', action: 'ingresar'
+                    flash.message = "Contraseña incorrecta"
+                    return
+                }
             }
         }
-
-        def admin = Admin.findByUsername(params.usernameL)
-        if (admin) {
-            if(admin.password == params.passwordL){
-                session.user = admin.username
-                session.tu = 'admin'
-                redirect controller: 'web', action: 'admin'
-                return
-            }
-            else{
-                redirect controller: 'web', action: 'ingresar'
-                flash.message = "Contraseña incorrecta"
-                return
+        else if(aux.equals("Administrador")) {
+            def admin = Admin.findByUsername(params.usernameL)
+            if (admin) {
+                if (admin.password == params.passwordL) {
+                    session.user = admin.username
+                    session.tu = 'admin'
+                    redirect controller: 'web', action: 'admin'
+                    return
+                } else {
+                    redirect controller: 'web', action: 'ingresar'
+                    flash.message = "Contraseña incorrecta"
+                    return
+                }
             }
         }
-
-        def restaurant = Restaurant.findByUsername(params.usernameL)
-        if (restaurant) {
-            if(restaurant.password == params.passwordL){
-                session.user = restaurant.username
-                session.tu = false
-                redirect controller: 'web', action: 'index'
-            }
-            else{
-                redirect controller: 'web', action: 'ingresar'
-                flash.message = "Contraseña incorrecta"
+        else if(aux.equals("Restaurante")) {
+            def restaurant = Restaurant.findByUsername(params.usernameL)
+            if (restaurant) {
+                if (restaurant.password == params.passwordL) {
+                    session.user = restaurant.username
+                    session.tu = false
+                    redirect controller: 'web', action: 'index'
+                    return
+                } else {
+                    redirect controller: 'web', action: 'ingresar'
+                    flash.message = "Contraseña incorrecta"
+                    return
+                }
             }
         }
         else {
             redirect controller: 'web', action: 'ingresar'
-            flash.message = "No existe el usuario"
+            flash.message = "No existe el usuario o la contraseña está mal escrita"
+            return
         }
     }
 
@@ -98,7 +102,7 @@ class UserController {
 
     private LDAPConnection lc = new LDAPConnection();
 
-    public String login2(String nombreUsuario, String contrasena){
+    String login2(String nombreUsuario, String contrasena){
 
         System.out.println("DATOS ---> " + nombreUsuario + " - " + contrasena);
 
@@ -115,45 +119,45 @@ class UserController {
 
     }
 
-    public Boolean conectar(){
+    Boolean conectar(){
 
-        String ldapHost = "192.168.0.5";
-        String dn = "cn=admin,dc=culinaire,dc=com";
-        String password = "culinaire";
+        String ldapHost = "192.168.0.5"
+        String dn = "cn=admin,dc=culinaire,dc=com"
+        String password = "culinaire"
 
-        int ldapPort =  LDAPConnection.DEFAULT_PORT;
-        int ldapVersion = LDAPConnection.LDAP_V3;
+        int ldapPort =  LDAPConnection.DEFAULT_PORT
+        int ldapVersion = LDAPConnection.LDAP_V3
 
         try {
             lc.connect(ldapHost, ldapPort);
-            System.out.println("====Conectado al Servidor LDAP====");
-            lc.bind(ldapVersion, dn, password.getBytes("UTF8"));
-            System.out.println("====Autenticado en el servidor====");
-            return true;
+            System.out.println("====Conectado al Servidor LDAP====")
+            lc.bind(ldapVersion, dn, password.getBytes("UTF8"))
+            System.out.println("====Autenticado en el servidor====")
+            return true
         } catch (LDAPException | UnsupportedEncodingException ex) {
-            System.out.println("====ERROR al conectarse al Servidor LDAP====");
-            return false;
+            System.out.println("====ERROR al conectarse al Servidor LDAP====")
+            return false
         }
 
     }
 
-    public String validarContrasena(String nombreUsuario, String contrasena){
+    String validarContrasena(String nombreUsuario, String contrasena){
 
-        String dn = "cn="+nombreUsuario+",ou=Usuarios,dc=culinaire,dc=com";
+        String dn = "cn="+nombreUsuario+",ou=Usuarios,dc=culinaire,dc=com"
         try {
-            lc.bind(dn, contrasena);
-            System.out.println("====Contrasena Validada====");
-            String aux = lc.read(dn).toString();
-            System.out.println(aux);
+            lc.bind(dn, contrasena)
+            System.out.println("====Contrasena Validada====")
+            String aux = lc.read(dn).toString()
+            System.out.println(aux)
             if(aux.contains("value='500'"))
-                return "Administrador";
+                return "Administrador"
             if(aux.contains("value='501'"))
-                return "Usuario";
+                return "Usuario"
             else
-                return "Restaurante";
+                return "Restaurante"
         } catch (LDAPException ex) {
             System.out.println("====ERROR al validar la contrasena====");
-            return "error";
+            return "error"
         }
 
     }
